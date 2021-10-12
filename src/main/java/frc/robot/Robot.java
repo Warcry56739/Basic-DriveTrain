caeaf48fb71b6dd4d7bd011e49feb1ba50ccf9b0
RachelMo8068 <52724995+RachelMo8068@users.noreply.github.com>
2021-10-12 18:42:43 -0400

    code template added

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/**
+ * Import statements
+ * ~ All the classes you need to make the robot work ~ 
+ */
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -20,125 +24,61 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
- * Sourced from WPILib's Arcade Drive example, Rev's CAN Spark example, and some guessing
+ * Robot Class, where all your code goes.
+ * all of your comments should have an example underneath of it to help you
+ * 
+ * NOTE: standard naming conventions: m_name
+ * Why? IDK that's just the way its done
+    * e.g m_robotdrive
+          m_joystick
  */
 public class Robot extends TimedRobot {
 
-  //Motor Controller CAN Ids
-  public static final int leftID1 = 0;
-  public static final int leftID2 = 1;
-  public static final int rightID1 = 2;
-  public static final int rightID2 = 3;
+  //if you want any variables put them here
+  //public static final int name = value;
   
-  //Left Side Motor Controllers
-  private final CANSparkMax m_leftlead = new CANSparkMax(leftID1, MotorType.kBrushless);  
-  private final CANSparkMax m_leftfollow = new CANSparkMax(leftID2, MotorType.kBrushless);
-  
-  //Right Side Motor Controllers
-  
-  private final CANSparkMax m_rightlead = new CANSparkMax(rightID1, MotorType.kBrushless);
-  private final CANSparkMax m_rightfollow = new CANSparkMax(rightID2, MotorType.kBrushless);
 
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftlead, m_rightlead);
-  private final Joystick m_stick = new Joystick(0);
+  /**
+   * Initialize your motor controllers here
+   * 
+   * Pop quiz! How many motor controllers do we have? Make sure you initialize all of them
+   */
 
+  // private final CANSparkMax motorcntrl_name = new CANSparkMax(cntrlID, MotorType.kBrushless);  
+  
+  /*Initialize your drivetrain here
+  * private final DifferentialDrive name = new DifferentialDrive( left , right );
+  * NOTE: you can only have one left side and one right side motor controller in the drivetrain object
+  */
+
+  //Initialize your Joystick here
+  //private final Joystick name = new Joystick(port);
+
+  /**
+   * Pop quiz! When does this method run?
+   */
   @Override
   public void robotInit() {
   }
 
+  /**
+   * Pop quiz! When does this method run?
+   */
   @Override
   public void teleopPeriodic() {
-    // Drive with arcade drive.
-    // That means that the Y axis drives forward
-    // and backward, and the X turns left and right.
-    m_leftfollow.follow(m_leftlead);
-    m_rightfollow.follow(m_rightlead);
-    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    //We want to drive with arcade drive!
+    /**
+     * NOTE: If you have more than 2 motor controllers (I'm not saying you should or should not but if you do)
+     * you need to make a "lead" motor for each side (left and right) that will receive the commands and the other
+     * ones need to "follow" those motors
+     * 
+     * If you only have 2 motor controllers then you're golden!
+     */
+    
+     //follow cmd
+     //motorcntrl.follow(leadercntrl);
+
+     //make it drive
+     //drivetrain_name.arcadeDrive(speed, rotation);
   }
 }
-
-/*
-package frc.robot;
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-public class Robot extends TimedRobot {
-
-  //Joysticks
-  Joystick leftJoy = new Joystick(0);
-  Joystick rightJoy = new Joystick(1);
-
-  //Motor Controllers (Left Side)
-  Spark sparkL1 = new Spark(0);
-  Spark sparkL2 = new Spark(1);
-  Spark sparkL3 = new Spark(2);
-
-  //Motor Controllers (Right Side)
-  Spark sparkR1 = new Spark(3);
-  Spark sparkR2 = new Spark(4);
-  Spark sparkR3 = new Spark(5);
-
-  SendableChooser<Number> speed = new SendableChooser<>();
-
-  @Override
-  public void robotInit() {
-    SmartDashboard.putBoolean("Reverse Left Drivetrain?", false);
-    SmartDashboard.putBoolean("Reverse Right Drivetrain?", true);
-    SmartDashboard.putNumber("Speed Percentage", 100);
-  }
-
-  @Override
-  public void autonomousInit() {
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-  }
-
-  @Override
-  public void teleopInit() {
-  }
-
-  @Override
-  public void teleopPeriodic() {
-
-    double driveSpeed = SmartDashboard.getNumber("Speed Percentage", 100)/100;
-    
-  //The robot may drive backwards or spin around due to different drivetrain configurations.
-  //Play with these SmartDashboard parameters so it drives correctly.
-    if(SmartDashboard.getBoolean("Reverse Left Drivetrain?", true)){
-      sparkL1.set(-leftJoy.getY() * driveSpeed);
-      sparkL2.set(-leftJoy.getY() * driveSpeed);
-      sparkL3.set(-leftJoy.getY() * driveSpeed);
-    } else {
-      sparkL1.set(leftJoy.getY() * driveSpeed);
-      sparkL2.set(leftJoy.getY() * driveSpeed);
-      sparkL3.set(leftJoy.getY() * driveSpeed);
-    }
-
-    if(SmartDashboard.getBoolean("Reverse Right Drivetrain?", false)){
-      sparkR1.set(-rightJoy.getY() * driveSpeed);
-      sparkR2.set(-rightJoy.getY() * driveSpeed);
-      sparkR3.set(-rightJoy.getY() * driveSpeed);
-    } else {
-      sparkR1.set(rightJoy.getY() * driveSpeed);
-      sparkR2.set(rightJoy.getY() * driveSpeed);
-      sparkR3.set(rightJoy.getY() * driveSpeed);
-    }
-    
-  }
-
-  @Override
-  public void testInit() {
-  }
-
-  @Override
-  public void testPeriodic() {
-  }
-
-}
-*/
