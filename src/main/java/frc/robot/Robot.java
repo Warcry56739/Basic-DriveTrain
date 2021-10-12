7a6416b23df07667c579781a39cbaad0ce36572a
RachelMo8068 <52724995+RachelMo8068@users.noreply.github.com>
2021-10-12 19:41:37 -0400

    rookies made code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,11 +14,9 @@
  * ~ All the classes you need to make the robot work ~ 
  */
 package frc.robot;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -36,35 +34,43 @@ public class Robot extends TimedRobot {
 
   //if you want any variables put them here
   //public static final int name = value;
-  
-
+  public static final int left=
+  public static final int right=
   /**
    * Initialize your motor controllers here
    * 
    * Pop quiz! How many motor controllers do we have? Make sure you initialize all of them
-   */
+   *4 motor controllers
+   */ 
 
-  // private final CANSparkMax motorcntrl_name = new CANSparkMax(cntrlID, MotorType.kBrushless);  
+  // private  final CANSparkMax motorcntrl_name = new CANSparkMax(cntrlID, MotorType.kBrushless);  
+  private final CANSparkMax m_left1 = new CANSparkMax(0, MotorType.kBrushless);
+  private final CANSparkMax m_left2= new CANSparkMax(1, MotorType.kBrushless);
+  private final CANSparkMax m_right1= new CANSparkMax(2, MotorType.kBrushless);
+  private final CANSparkMax m_right2= new CANSparkMax(3,MotorType.kBrushless);
   
   /*Initialize your drivetrain here
   * private final DifferentialDrive name = new DifferentialDrive( left , right );
   * NOTE: you can only have one left side and one right side motor controller in the drivetrain object
   */
-
+  private final DifferentialDrive Cbt = new DifferentialDrive(m_left1 , m_right1);
+  
   //Initialize your Joystick here
   //private final Joystick name = new Joystick(port);
+  private final Joystick CBJ = new Joystick(0);
+  /**
+   * Pop quiz! When does this method run?
+   * When the code starts/
+   * */
+  @Override
+  public void robotInit() {}
 
   /**
    * Pop quiz! When does this method run?
-   */
-  @Override
-  public void robotInit() {
-  }
-
-  /**
-   * Pop quiz! When does this method run?
-   */
-  @Override
+   * during the teleop period
+   * */
+  
+   @Override
   public void teleopPeriodic() {
     //We want to drive with arcade drive!
     /**
@@ -77,8 +83,11 @@ public class Robot extends TimedRobot {
     
      //follow cmd
      //motorcntrl.follow(leadercntrl);
-
+     m_left2.follow(m_left1);
+     m_right2.follow(m_right1);
      //make it drive
      //drivetrain_name.arcadeDrive(speed, rotation);
+    Cbt.arcadeDrive(CBJ.getY(), CBJ.getX());
+    
   }
 }
